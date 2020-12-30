977ec235fb6ac578071864289d66603936a7bd16
FancisLuo <luojianyong6@live.com>
2020-12-30 23:17:20 +0800

    抛物线运动

---
+++ 11-ObliqueMovement/ObliqueEquationMoveScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObliqueEquationMoveScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _height;
+
+    private float _distance;
+    [SerializeField]
+    private float _moveSpeed;
+    [SerializeField]
+    private GameObject _moveObject;
+    [SerializeField]
+    private GameObject _target;
+
+    private float _a;
+    private float _b;
+
+    private float _toMoveTime;
+
+    private float _vx;
+    private float _vz;
+
+    private float _timer;
+
+    private Vector3 _originPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+        CalculateCoff();
+        _originPosition = _moveObject.transform.position;
+
+        _timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer >= _toMoveTime)
+            return;
+
+        float dt = Time.deltaTime;
+        float dsx = _vx * dt;
+        float dsz = _vz * dt;
+
+        var pos = _moveObject.transform.position;
+        pos.x += dsx;
+        pos.z += dsz;
+
+        var dir = pos - _originPosition;
+        var sqrtDist = dir.x * dir.x + dir.z * dir.z;
+        var ds = Mathf.Sqrt(sqrtDist);
+        pos.y = GetHeight(ds);
+
+        _moveObject.transform.position = pos;
+        _timer += dt;
+    }
+
+    private void Init()
+    {
+        _distance = Vector3.Distance(_moveObject.transform.position, _target.transform.position);
+        _moveObject.transform.LookAt(_target.transform.position);
+        _toMoveTime = _distance / _moveSpeed;
+
+        var dir = _target.transform.position - _moveObject.transform.position;
+        dir.Normalize();
+        var thelta = Vector3.Angle(dir, new Vector3(1, 0, 0));
+        _vz = Mathf.Sin(Mathf.Deg2Rad * thelta) * _moveSpeed;
+        _vx = Mathf.Cos(Mathf.Deg2Rad * thelta) * _moveSpeed;
+    }
+
+    private void CalculateCoff()
+    {
+        _a = -(4 * _height) / (_distance * _distance);
+        _b = 4 * _height / _distance;
+    }
+
+    private float GetHeight(float ds)
+    {
+        float y = _a * ds * ds + _b * ds;
+        return y;
+    }
+}
---
+++ 11-ObliqueMovement/ObliqueMoveScript.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObliqueMoveScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _moveObject;
+
+    [SerializeField]
+    private GameObject _target;
+
+    [SerializeField]
+    private float _g = 9.8f;
+
+    [SerializeField]
+    private float _speed = 10;
+
+    private float _verticalSpeed;
+    private Vector3 _moveDirection;
+    private float _angleSpeed;
+    private float _angle;
+
+    private float _time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float tmepDistance = Vector3.Distance(_moveObject.transform.position, _target.transform.position);
+        float tempTime = tmepDistance / _speed;
+        float riseTime, downTime;
+        riseTime = downTime = tempTime / 2;
+        _verticalSpeed = _g * riseTime;
+        _moveObject.transform.LookAt(_target.transform.position);
+        float tempTan = _verticalSpeed / _speed;
+        double hu = Math.Atan(tempTan);
+        _angle = (float)(180 / Math.PI * hu);
+        _moveObject.transform.eulerAngles = new Vector3(-_angle, _moveObject.transform.eulerAngles.y, _moveObject.transform.eulerAngles.z);
+        _angleSpeed = _angle / riseTime;
+        _moveDirection = _target.transform.position - _moveObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_moveObject.transform.position.y < _target.transform.position.y)
+        {
+            //finish  
+            return;
+        }
+
+        _time += Time.deltaTime;
+        float test = _verticalSpeed - _g * _time;
+        _moveObject.transform.Translate(_moveDirection.normalized * _speed * Time.deltaTime, Space.World);
+        _moveObject.transform.Translate(Vector3.up * test * Time.deltaTime, Space.World);
+        float testAngle = -_angle + _angleSpeed * _time;
+        _moveObject.transform.eulerAngles = new Vector3(testAngle, _moveObject.transform.eulerAngles.y, _moveObject.transform.eulerAngles.z);
+    }
+}
